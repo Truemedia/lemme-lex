9faa649aae2987d2b00e2efa97b04847a7323491
Wade Penistone <wade@tribusdigital.com>
2018-10-22 20:54:50 +0100

    Read and writing lexicon files

--- src/index.js
+++ src/index.js
@@ -1,23 +1,113 @@
+const fs = require('fs');
+const path = require('path');
+const Json2csvParser = require('json2csv').Parser;
+const jsonfile = require('jsonfile');
+const lineReader = require('line-reader');
+
 class Lexicon
 {
     constructor(locale = 'en_GB')
     {
-        this.lexemes = [];
+      this.lexemes = [];
     }
 
     addEntry(lexeme)
     {
-        this.lexemes.push(lexeme);
+      this.lexemes.push(lexeme);
     }
 
     addEntries(lexemes)
     {
-        this.lexemes.push(...lexemes);
+      this.lexemes.push(...lexemes);
     }
 
-    toJson()
+    /**
+      * Get lexicon as list
+      */
+    get list()
     {
-        return this.lexemes.map( (lexeme) => lexeme.toJson() );
+      return this.lexemes.map(lexeme => { return lexeme.flat; });
+    }
+
+    /**
+      * Get lexicon as object
+      */
+    get object()
+    {
+      return Object.assign(...this.json);
+    }
+
+    /**
+      * Get lexicon as JSON
+      */
+    get json()
+    {
+      return this.lexemes.map( (lexeme) => lexeme.json );
+    }
+
+    static fromFile(filename)
+    {
+      let basename = path.basename(filename);
+      let [name, ext] = basename.split('.');
+      switch (ext)
+      {
+        case 'json':
+          return jsonfile.readFile(filename)
+            .then(obj => {
+              let instance = new this;
+              instance.addEntries(
+                Object.entries(obj).map(entry => {
+                  let [name, tags] = entry;
+                  return new Lexeme(name, tags);
+                })
+              );
+              return instance;
+            });
+        break;
+        case 'txt':
+          return new Promise(function(resolve, reject) {
+            let instance = new this;
+            lineReader.eachLine(filename, function(line, last) {
+              let values = line.split('" "').map(value => value.replace('"', ''));
+              let name = values.shift();
+              instance.addEntry( new Lexeme(name, values) );
+              if (last) {
+                resolve(instance);
+              }
+            });
+          });
+        break;
+      }
+      return new this();
+    }
+
+    get txt()
+    {
+      let opts = {header: false, delimiter: ' '};
+      let parser = new Json2csvParser(opts);
+      let txt = parser.parse(this.list);
+      return txt;
+    }
+
+    toFile(filename)
+    {
+      let basename = path.basename(filename);
+      let [name, ext] = basename.split('.');
+      switch (ext)
+      {
+        case 'json':
+          jsonfile.writeFile(filename, this.object, { spaces: 2 }, function(err) {
+            if (err) console.error(err);
+            console.log(`JSON file saved to disk: ${filename}`);
+          });
+        break;
+        case 'txt':
+          fs.writeFile(filename, this.txt, function(err) {
+            if (err) console.log(err);
+            console.log(`Text file saved to disk: ${filename}`);
+          });
+        break;
+      }
     }
 }
 
@@ -29,12 +119,26 @@ class Lexeme
         this.tags = tags;
     }
 
-    toJson()
+    /**
+      * Get lexeme as flat array [name, ...tags]
+      */
+    get flat()
     {
-        return {[this.word]: this.tags};
+      return [this.word].concat(this.tags);
     }
 
-    toRecord()
+    /**
+      * Get lexeme as object {name: tags}
+      */
+    get json()
+    {
+      return {[this.word]: this.tags};
+    }
+
+    /**
+      * Get lexeme as record (suitable for db or api) {name, tags}
+      */
+    get record()
     {
       return {
         name: this.word, tags: this.tags
--- test.js
+++ test.js
@@ -3,4 +3,17 @@ const {Lexicon, Lexeme} = require('./src/index');
 let phrase = new Lexeme('hello there', ['ArrivalSalutation']);
 let lex = new Lexicon();
 lex.addEntry(phrase);
-console.log(lex.toJson());
+// To JSON
+console.log(lex.json);
+// To json file
+lex.toFile('test.json');
+// To text file
+lex.toFile('test.txt');
+// From json file
+Lexicon.fromFile('test.json').then(instance => {
+  console.log('instance', instance);
+});
+// From txt file
+Lexicon.fromFile('test.txt').then(instance => {
+  console.log('instance', instance);
+});
